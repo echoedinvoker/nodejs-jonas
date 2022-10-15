2d26e364d90d5a5a25ecc0e4e298cc065fe2ddf3
echoedinvoker <echoedinvoker@gmail.com>
2022-10-16 05:21:03 +0800

    qq

---
+++ 6-express/56-DELETE/app.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+
+  res.status(200).json({
+    status: "sucess",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  newTours = [...tours, newTour];
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(newTours),
+    (err) => {
+      // res.json(newTour);
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length)
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+
+  const tour = Object.assign(
+    tours.find((el) => el.id === id),
+    req.body
+  );
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length)
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+
+  // const tour = Object.assign(
+  //   tours.find((el) => el.id === id),
+  //   req.body
+  // );
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
---
+++ 6-express/56-DELETE/memo.md
@@ -0,0 +1,10 @@
+## **Delete router**
+
+![Alt copty from patch and modify it](pic/bandicam%202022-10-16%2005-11-17-285.jpg)
+
+- The concept of **delete** is very similar to **patch**, the only difference is that we don't need to write the logic of update tour, so we can choose to **copy** directly from the patch router.
+- But again, we don't make any changes to our server's files here, but rather to the response part of the process.
+
+## **DELETE Request from Postman**
+
+![Alt  postman request](pic/bandicam%202022-10-16%2005-14-15-098.jpg)
