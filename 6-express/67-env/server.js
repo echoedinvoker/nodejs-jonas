948cdaaf999ca088584e881660886f53099d9de6
echoedinvoker <echoedinvoker@gmail.com>
2022-10-21 06:16:43 +0800

    qq

--- 6-express/67-env/server.js
+++ 6-express/67-env/server.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/config.env` });
 const app = require("./app");
 
-console.log(process.env.NODE_ENV);
+console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
