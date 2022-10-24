44bd31d8ddf6ec69aacc23070892f00263c36eb0
echoedinvoker <echoedinvoker@gmail.com>
2022-10-24 12:10:55 +0800

    qq

--- 8-mongoose/90-read-doc/controllers/tourControllers.js
+++ 8-mongoose/90-read-doc/controllers/tourControllers.js
@@ -1,5 +1,42 @@
 const Tour = require('../models/tourModel');
 
+exports.getTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    // const tour = await Tour.findOne({ _id: req.params.id });
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
 exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
@@ -16,18 +53,6 @@ exports.createTour = async (req, res) => {
       message: err,
     });
   }
-};
-
-exports.getTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
-};
-
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
 };
 
 exports.patchTour = (req, res) => {
