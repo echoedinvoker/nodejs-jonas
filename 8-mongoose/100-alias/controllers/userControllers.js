4f25821211d982f749e2d3beabda55cc2e8abe0b
echoedinvoker <echoedinvoker@gmail.com>
2022-11-11 09:44:46 +0800

    qq

---
+++ 8-mongoose/100-alias/controllers/userControllers.js
@@ -0,0 +1,35 @@
+// Router handlers for Users
+exports.getUsers = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This router is not defined yet!",
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This router is not defined yet!",
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This router is not defined yet!",
+  });
+};
+
+exports.patchUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This router is not defined yet!",
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: "error",
+    message: "This router is not defined yet!",
+  });
+};
---
+++ 8-mongoose/100-alias/memo.md
@@ -0,0 +1,15 @@
+## **Popular Query URL**
+
+![Alt popular query URL](pic/bandicam%202022-11-11%2009-12-24-908.jpg)
+
+## **Build Alias Router**
+
+![Alt pass middleware into get](pic/bandicam%202022-11-11%2009-20-00-666.jpg)
+
+## **Build Middleware for Alias**
+
+![Alt build middleware](pic/bandicam%202022-11-11%2009-29-55-301.jpg)
+
+## **Analyze and Test**
+
+![Alt import middleware into tourRouters.js](pic/bandicam%202022-11-11%2009-38-25-286.jpg)
