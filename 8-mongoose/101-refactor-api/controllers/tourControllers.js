c401a78d44270d92a7e12402195e3145197cad8f
echoedinvoker <echoedinvoker@gmail.com>
2022-11-11 15:49:36 +0800

    qq

---
+++ 8-mongoose/101-refactor-api/controllers/tourControllers.js
@@ -0,0 +1,113 @@
+const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next();
+};
+
+exports.getTours = async (req, res) => {
+  try {
+    // EXECUTE QUERY (PAGINATION, SORTING, SELECT...)
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      number: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    // const tour = await Tour.findOne({ _id: req.params.id });
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.patchTour = async (req, res) => {
+  try {
+    // find specific document by idy, then update it
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    // find specific document by idy, then delete it
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
---
+++ 8-mongoose/101-refactor-api/memo.md
@@ -0,0 +1,35 @@
+## **Goal**
+
+![Alt purpose describe](pic/bandicam%202022-11-11%2014-32-25-242.jpg)
+
+## **Basic frame of class APIFeatures**
+
+![Alt APIFeatures class](pic/bandicam%202022-11-11%2014-45-20-511.jpg)
+
+## **Build Methods of class APIFeatures**
+
+![Alt filter()](pic/bandicam%202022-11-11%2014-59-01-026.jpg)
+
+![Alt sort()](pic/bandicam%202022-11-11%2015-03-22-734.jpg)
+
+![Alt limitFields()](pic/bandicam%202022-11-11%2015-06-32-678.jpg)
+
+![Alt paginate()](pic/bandicam%202022-11-11%2015-12-24-060.jpg)
+
+## **Implement API features with APIFeatures class**
+
+![Alt execute plan](pic/bandicam%202022-11-11%2015-15-53-756.jpg)
+
+![Alt implement API features](pic/bandicam%202022-11-11%2015-23-09-205.jpg)
+
+![Alt middle test](pic/bandicam%202022-11-11%2015-25-19-650.jpg)
+
+## **Separate APIFeature class out**
+
+![Alt utils](pic/bandicam%202022-11-11%2015-31-23-415.jpg)
+
+![Alt import back](pic/bandicam%202022-11-11%2015-35-32-033.jpg)
+
+## **Select desired API features easily**
+
+![Alt choose features](pic/bandicam%202022-11-11%2015-39-11-451.jpg)
