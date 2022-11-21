d39c8732ae4fc6bad205c91f4ef57c3353d28f11
echoedinvoker <echoedinvoker@gmail.com>
2022-11-22 00:18:41 +0800

    qq

---
+++ 8-mongoose/107-aggregation-middleware/memo.md
@@ -0,0 +1,21 @@
+## **Recall memory about Aggregation**
+
+![Alt aggr in controller](pic/01.jpg)
+
+![Alt send request by P](pic/02.jpg)
+
+## **Intro Aggregation Middleware and Print Pipeline in the console**
+
+![Alt test aggr middleware by console](pic/03.jpg)
+
+![Alt send request by P](pic/04.jpg)
+
+![Alt pipeline in the console](pic/05.jpg)
+
+## **Unshift New Stage into Pipeline by Aggregation Middleware**
+
+![Alt unshift stage](pic/06.jpg)
+
+![Alt new result](pic/07.jpg)
+
+![Alt two match stages](pic/08.jpg)
---
+++ 8-mongoose/107-aggregation-middleware/models/tourModel.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      require: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
+    __v: {
+      type: Number,
+      select: false,
+    },
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('find', function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
