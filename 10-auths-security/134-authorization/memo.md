a40bec7f61c601b35b7733354b2d8d36defb6c07
echoedinvoker <echoedinvoker@gmail.com>
2022-12-05 11:57:11 +0800

    qq

---
+++ 10-auths-security/134-authorization/memo.md
@@ -0,0 +1,23 @@
+## **Reason to Restrict Role for Routers**
+
+![Alt goal](pic/12.jpg)
+
+## **Routers**
+
+![Alt restrictTo](pic/13.jpg)
+
+## **Add new field "role" into user Schema**
+
+![Alt role](pic/14.jpg)
+
+## **restrictTo: How to pass parameters into Middleware**
+
+![Alt multiple roles pass into restrictTo](pic/15.jpg)
+
+![Alt return middleware](pic/16.jpg)
+
+## **Test**
+
+![Alt signup new user](pic/17.jpg)
+
+![Alt delete tour](pic/18.jpg)
---
+++ 10-auths-security/134-authorization/server.js
@@ -0,0 +1,49 @@
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log(`💥Error Name💥: ${err.name}`);
+  console.log(`💥💥Error Text: ${err.message}`);
+  console.log('UNCAUGHT EXCEPTION! Shutting down!');
+
+  process.exit(1);
+});
+
+dotenv.config({ path: `${__dirname}/config.env` });
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  const fullMessage = err.message;
+  const errmsgStart = 0; // Start at the beginnning
+  const newline = /\n/; // new line character
+  const errmsgStop = fullMessage.search(newline); // Find new line
+  const errmsgLen = errmsgStop - errmsgStart;
+  const errorText = fullMessage.substr(errmsgStart, errmsgLen);
+  console.log(`💥Error Name💥: ${err.name}`);
+  console.log(`💥💥Error Text: ${errorText}`);
+  console.log('UNHANDLED REJECTION! Shutting down!');
+  server.close(() => {
+    process.exit(1);
+  });
+});
